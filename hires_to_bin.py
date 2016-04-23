ca7470cf10e10913da891963253d44854eb987d1
Agnes Heyer <agnesheyer@gmail.com>
2016-04-23 18:20:31 +0200

    Example image and readme

--- hires_to_bin.py
+++ hires_to_bin.py
@@ -7,29 +7,10 @@ from math import *
 import os.path
 
 
-# Opens an 8-bit indexed image 320 x 200 pixels in size and converts the image
-# into Commodore 64 bitmap data and color data. The first 16 colors are assumed
-# to be in the same order as the C64 palette.
-#
-# Color order:
-#
-#   0x0 Black    0x8 Orange
-#   0x1 White    0x9 Brown
-#   0x2 Red      0xA Light red
-#   0x3 Cyan     0xB Dark gray
-#   0x4 Purple   0xC Mid gray
-#   0x5 Green    0xD Light green
-#   0x6 Blue     0xE Light blue
-#   0x7 Yellow   0xF Light gray
-#
-# The error indicator image will use the first three colors in the palette
-# (assumed to be black, white, red).
-
-
 def main():
 
     if len(sys.argv) < 3:
-        sys.exit('Usage: hirestobin.py [IMGFILE] [BINFILE]')
+        sys.exit('Usage: hires_to_bin.py [IMGFILE] [BINFILE]')
     else:
         infile = sys.argv[1]
         outfile = sys.argv[2]
